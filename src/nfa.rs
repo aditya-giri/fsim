4234e312f29910446e26af518c8c4102ad0115c6
Aditya Giridharan <aditya.giridharan@gmail.com>
2026-02-25 20:43:26 -0500

    validate nfa

--- src/nfa.rs
+++ src/nfa.rs
@@ -29,6 +29,24 @@ impl NFA {
         alphabet: &HashSet<char>,
         tfn: &HashMap<(usize, char), usize>,
     ) -> Result<(), NFATypeError> {
+        if !(start < states) {
+            return Err(NFATypeError::InvalidStartState);
+        }
+        if !(accept.iter().all(|&s| s < states)) {
+            return Err(NFATypeError::InvalidAcceptState);
+        }
+        if alphabet.contains(&EPSILON) {
+            return Err(NFATypeError::ReservedCharacterInAlphabet);
+        }
+        if !tfn
+            .keys()
+            .all(|&(s, c)| s < states && (c == EPSILON || alphabet.contains(&c)))
+        {
+            return Err(NFATypeError::InvalidTransitionFunction);
+        }
+        if !tfn.values().all(|&s| s < states) {
+            return Err(NFATypeError::InvalidTransitionFunction);
+        }
         Ok(())
     }
 
@@ -48,12 +66,14 @@ impl NFA {
             .into_iter()
             .map(|(k, v)| ((State(k.0), k.1), State(v)))
             .collect();
+        let mut alphabet_with_epsilon = alphabet.clone();
+        alphabet_with_epsilon.insert(EPSILON);
 
         let nfa = Self {
             states,
             start,
             accept,
-            alphabet,
+            alphabet: alphabet_with_epsilon,
             tfn,
         };
 
@@ -69,5 +89,62 @@ mod tests {
     fn good_nfa_succeeds() {
         let _ = NFA::new(1, 0, HashSet::new(), HashSet::new(), HashMap::new()).unwrap();
         assert!(true);
+    }
+
+    #[test]
+    fn invalid_start_state_fails() {
+        let bad_nfa = NFA::new(0, 0, HashSet::new(), HashSet::new(), HashMap::new());
+        assert!(matches!(bad_nfa, Err(NFATypeError::InvalidStartState)));
+    }
+
+    #[test]
+    fn invalid_accept_state_fails() {
+        let bad_nfa = NFA::new(1, 0, HashSet::from([1]), HashSet::new(), HashMap::new());
+        assert!(matches!(bad_nfa, Err(NFATypeError::InvalidAcceptState)));
+    }
+
+    #[test]
+    fn invalid_transition_fn_bad_state_in_domain_fails() {
+        let mut tfn = HashMap::new();
+        tfn.insert((0, '0'), 1);
+        tfn.insert((0, '1'), 1);
+        tfn.insert((1, '0'), 0);
+        tfn.insert((1, '1'), 0);
+
+        tfn.insert((2, '0'), 1);
+        let bad_nfa = NFA::new(2, 0, HashSet::from([0]), HashSet::from(['0', '1']), tfn);
+        assert!(matches!(
+            bad_nfa,
+            Err(NFATypeError::InvalidTransitionFunction)
+        ));
+    }
+
+    #[test]
+    fn invalid_transition_fn_bad_alphabet_in_domain_fails() {
+        let mut tfn = HashMap::new();
+        tfn.insert((0, '0'), 1);
+        tfn.insert((0, '1'), 1);
+        tfn.insert((1, '0'), 0);
+        tfn.insert((1, '1'), 0);
+        tfn.insert((0, '2'), 0);
+        let bad_nfa = NFA::new(2, 0, HashSet::from([0]), HashSet::from(['0', '1']), tfn);
+        assert!(matches!(
+            bad_nfa,
+            Err(NFATypeError::InvalidTransitionFunction)
+        ));
+    }
+
+    #[test]
+    fn invalid_transition_fn_bad_state_in_range_fails() {
+        let mut tfn = HashMap::new();
+        tfn.insert((0, '0'), 1);
+        tfn.insert((0, '1'), 2);
+        tfn.insert((1, '0'), 0);
+        tfn.insert((1, '1'), 0);
+        let bad_nfa = NFA::new(2, 0, HashSet::from([0]), HashSet::from(['0', '1']), tfn);
+        assert!(matches!(
+            bad_nfa,
+            Err(NFATypeError::InvalidTransitionFunction)
+        ));
     }
 }
